7153137fce373f486eea70a10cd48c10257d20b7
HugoMab <hugo.mabillard@gmail.com>
2025-11-17 22:18:48 +0100

    Pour la production: ajout d'un graphique trimestriel Pour la consommation: ajout des graphiques hebdomadaires et mensuels + la moyenne sur les 7 derniers jours.

--- resultat_EcoFlow.r
+++ resultat_EcoFlow.r
@@ -170,6 +170,17 @@ ggplot() +
   theme_gray() +
   theme(axis.text.x = element_text(angle=90, vjust = 0.5, hjust = 1), plot.title = element_text(hjust=0.5))
 
+# Trimestriel
+ggplot() +
+  geom_col(data = prod_trim, aes(x = trim, y = prod_kWh), fill = "#ffd700", alpha = 0.85) +
+ # geom_hline(yintercept = mean_prod_mens, linetype = "dashed", color = "khaki4") +
+  labs(title = "Production trimestrielle (en kWh)",
+       x = "Trimestre",
+       y = "Production (kWh)") +
+  scale_x_discrete(breaks = seq(3,4,1)) +
+  theme_gray() +
+  theme(axis.text.x = element_text(angle=90, vjust = 0.5, hjust = 1), plot.title = element_text(hjust=0.5))
+
 
 ## Maximum heure par heure
 (max_heure <- df_long[, .(max_heure = max(Production, na.rm=TRUE)), by="Heure"][order(Heure)])
@@ -231,6 +242,19 @@ print(tab_moins)
 # Analyse de la consommation #
 ##############################
 
+## Format long
+df_prod_long <- conso_horaire %>%
+  pivot_longer(
+    cols = -c(date, an, mois, semaine, trim, jour), # Toutes les colonnes sauf dates
+    names_to = "Heure", # Nouvelle colonne pour les heures
+    values_to = "Consommation") %>%  # Valeurs de production horaire
+  mutate(
+    Heure = as.integer(Heure),
+    date = as.Date(date)
+  )
+
+df_prod_long <- as.data.table(df_prod_long)
+
 # Journée production plus élevée
 (date_max <- conso_quot[which.max(conso_quot[, consommation]), date])
 (date_min <- conso_quot[which.min(conso_quot[, consommation]), date])
@@ -242,16 +266,101 @@ print(tab_moins)
 
 # Quotidien
 ggplot(conso_quot, aes(x = date)) +
-  geom_bar(aes(y = consommation), stat = "identity", fill = "#a7a824") + 
-  geom_hline(yintercept = mean_conso_quot, linetype = "dotdash", color = "khaki4") +
+  geom_bar(aes(y = consommation), stat = "identity", fill = "#ca8f9c") + 
+  geom_hline(yintercept = mean_conso_quot, linetype = "dotdash", color = "violetred") +
   labs(x = "Date", y = "Consommation (en Wh)", title = "Consommation quotidienne")
 
 
-
 jour_ligne <- date_max
-jour_barre <- auj - 0
+jour_barre <- conso_quot[.N, date]
 #jour_barre <- "2025-05-24"
 
-df_ligne <- subset(df_long, date == jour_ligne)
-df_barre <- subset(df_long, date == jour_barre)
+df_ligne <- subset(df_prod_long, date == jour_ligne)
+df_barre <- subset(df_prod_long, date == jour_barre)
+
+# Graphique horaire
+ggplot() +
+  geom_col(data = df_barre, aes(x = Heure, y = Consommation), fill = "#ca8f9c", alpha = 0.75) +
+  geom_line(data = df_ligne, aes(x = Heure, y = Consommation), color = "violetred") +
+  geom_point(data = df_ligne, aes(x = Heure, y = Consommation), color = "violetred", size = 2) +
+  labs(title = "Production horaire des panneaux solaires",
+       x = "Heure",
+       y = "Consommation (Wh)", 
+       subtitle = paste("Barres =", jour_barre, "| Ligne =", jour_ligne)) +
+  scale_x_continuous(breaks = seq(0, 23, 1)) +
+  theme_gray() +
+  theme(axis.text.x = element_text(angle=90, vjust = 0.5, hjust = 1))
+
+
+## Résumés des consommations hebdomadaires, mensuels, timestrielles, etc.
+conso_hebd <- conso_quot[, .(conso_kWh = (sum(consommation)/1000)), by = c("semaine", "an")][order(semaine, an)]
+conso_hebd <- conso_hebd[2:.N, ]
+conso_mens <- conso_quot[, .(conso_kWh = (sum(consommation)/1000)), by = c("mois", "an")][order(mois, an)]
+conso_mens <- conso_mens[2:.N,]
+conso_trim <- conso_quot[, .(conso_kWh = (sum(consommation)/1000)), by = c("trim", "an")][order(trim, an)]
+conso_trim <- conso_trim[2:.N,]
+conso_an <- conso_quot[, .(conso_kWh = (sum(consommation)/1000)), by = "an"][order(an)]
+
+# Calcul d'une moyenne
+(mean_conso_hebd = mean(conso_hebd[, conso_kWh]))
+(mean_conso_mens = mean(conso_mens[, conso_kWh]))
+
+
+## Graphique
+# Hebdomadaire
+ggplot() +
+  geom_col(data = conso_hebd, aes(x = semaine, y = conso_kWh), fill = "#ca8f9c", alpha = 0.85) +
+  geom_hline(yintercept = mean_conso_hebd, linetype = "dashed", color = "violetred") +
+  labs(title = "Consommation hebdomadaire (en kWh)",
+       x = "Semaine",
+       y = "Consommation (kWh)") +
+  scale_x_continuous(labels = scales::number_format(accuracy = 1)) +
+  theme_gray() +
+  theme(axis.text.x = element_text(angle=90, vjust = 0.5, hjust = 1), plot.title = element_text(hjust=0.5))
+
+# Mensuel
+ggplot() +
+  geom_col(data = conso_mens, aes(x = mois, y = conso_kWh), fill = "#ca8f9c", alpha = 0.85) +
+ # geom_hline(yintercept = mean_conso_mens, linetype = "dashed", color = "ca8f9c") +
+  labs(title = "Consommation mensuelle (en kWh)",
+       x = "Mois",
+       y = "Consommation (kWh)") +
+  scale_x_continuous(labels = scales::number_format(accuracy = 1)) +
+  theme_gray() +
+  theme(axis.text.x = element_text(angle=90, vjust = 0.5, hjust = 1), plot.title = element_text(hjust=0.5))
+
+
+
+## Calcul d'une moyenne sur 7 jours
+moy_heure <- df_prod_long[date <= jour_barre & date > jour_barre - 7, .(moy_heure = mean(Consommation, na.rm = TRUE)), by="Heure"][order(Heure)]
+
+# Ajouter une colonne "jour" dans df_long (à faire avant le ggplot)
+df_prod_long$jour <- factor(
+  paste0("J-", as.numeric(jour_barre - df_prod_long$date)),
+  levels = paste0("J-", 1:7)
+)
+
+# Filtrer les données à 7 derniers jours
+df_7jours <- df_prod_long[df_prod_long$date %in% (jour_barre - 1:7), ]
+
+
+ggplot() +
+  geom_line(data = moy_heure, aes(x = Heure, y = moy_heure), color = "#ffd700", linetype = "solid", linewidth = 1) +
+  geom_line(data = df_7jours, aes(x = Heure, y = Consommation, color = jour), linetype = "dashed", linewidth = 0.5) +
+  scale_color_manual(
+    name = "Jour",
+    values = c(
+      "J-1" = "green",
+      "J-2" = "red",
+      "J-3" = "gray14",
+      "J-4" = "tomato4",
+      "J-5" = "gainsboro",
+      "J-6" = "deepskyblue3",
+      "J-7" = "darkorchid2"
+    )
+  ) +
+  labs(title = "Consommation électrique, 7 derniers jours", x = "Heure",y = "Consommation (en Wh)") +
+  scale_x_continuous(breaks = seq(0, 23, 1)) +
+  theme_gray() +
+  theme(axis.text.x = element_text(angle=90, vjust = 0.5, hjust = 1), legend.position = "bottom")
 
